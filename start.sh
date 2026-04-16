f790de1ee1c286e6aeaff62637753acddef59bf8
e4te-devs <willkoala6@outlook.com>
2026-04-16 16:51:15 +0000

    start script

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+echo start
 cd dogeub
-docker run -p 0.0.0.0:3000:3000 doge
+echo running...
+dogeub = $(docker run -p 0.0.0.0:3000:3000 doge)
+if [$dogeub != 0] then
+    echo building first...
+    docker build -t doge .
+    echo running...
+    docker run -p 0.0.0.0:3000:3000 doge
+    echo Done.
+fi
