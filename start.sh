3f0b5d79531010c7b250b3b6a271181572faca57
e4te-devs <willkoala6@outlook.com>
2026-04-21 08:13:53 +0000

    added stuff to readme

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@ echo start
 cd dogeub
 echo running...
 dogeub = $(docker run -p 0.0.0.0:3000:3000 doge)
-if [$dogeub != 0] then
-    echo building first...
-    docker build -t doge .
-    echo running...
-    docker run -p 0.0.0.0:3000:3000 doge
-    echo Done.
+if [$dogeub != 0] then:
+    echo building first...;
+    docker build -t doge .;
+    echo running...;
+    docker run -p 0.0.0.0:3000:3000 doge;
+    echo Done;
 fi
