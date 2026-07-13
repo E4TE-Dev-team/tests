c603b55a32ac61eac3ca170e488b5505d480832c
e4te-devs <willkoala6@outlook.com>
2026-07-13 15:26:31 +0000

    Tailscale

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ DogeUB
 
 On codespace creation run
 ```bash
-git submodule init; git submodule update
+git submodule init; git submodule update; curl -fsSL https://tailscale.com/install.sh | sh; sudo tailscaled && sudo tailscale up
 ```
 Then just run:
 ```bash
--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-echo start
+echo start.sh: start
+sudo tailscaled &&
+echo start.sh: tailscaled started
+sudo tailscale up
+echo start.sh: tailscale up
 cd dogeub
-echo running...
+echo start.sh: running...
 docker build -t doge .
-docker run -p 0.0.0.0:3000:3000 doge
+docker run -p 0.0.0.0:3000:3000 doge && bash
