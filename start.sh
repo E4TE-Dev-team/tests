d8cce0d65c192f0eb269b1495f91a438cdefe9df
e4te-devs <willkoala6@outlook.com>
2026-07-16 08:47:50 +0000

    tailscale fix

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo start.sh: start
-sudo tailscaled && echo start.sh: tailscaled started & sudo tailscale up & echo start.sh: tailscale up & cd dogeub & echo start.sh: running...  & docker build -t doge .  &  docker run -p 0.0.0.0:3000:3000 doge && bash
+sudo tailscaled && echo start.sh: tailscaled started & sudo tailscale up & echo start.sh: tailscale up & cd dogeub & echo start.sh: running...  & docker build  --no-cache -t doge .  &  docker run -p 0.0.0.0:3000:3000 doge && bash
