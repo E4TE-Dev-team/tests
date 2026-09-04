031d16a9887ed886f12b1433fd91b8bfad0a9226
E4tTE-dev-George <25fisherg@cedarsupper.co.uk>
2026-09-04 11:21:56 +0000

    fixy

--- start.sh
+++ start.sh
@@ -2,5 +2,9 @@
 echo start
 cd dogeub
 echo running...
-docker build -t doge .
-docker run -p 0.0.0.0:3000:3000 doge
+docker run -d \
+  --name doge_container \
+  --restart unless-stopped \
+  -p 3000:3000 \
+  doge
+
